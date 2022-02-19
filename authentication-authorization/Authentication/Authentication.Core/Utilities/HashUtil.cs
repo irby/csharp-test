731c47febb57626cb5959619ba22707174397bb1
Matt Irby <matt.irby@outlook.com>
2022-02-19 11:23:48 -0500

    Update hash and salt size

--- authentication-authorization/Authentication/Authentication.Core/Utilities/HashUtil.cs
+++ authentication-authorization/Authentication/Authentication.Core/Utilities/HashUtil.cs
@@ -7,33 +7,35 @@ namespace Authentication.Core.Utilities
 {
     public static class HashUtil
     {
+        private const int HashSize = 64;
+        private const int SaltSize = 32;
         public static string HashPassword(string password, byte[] salt)
         {
             var hash = GetHash(password, salt);
-            var hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
+            var hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
             return Convert.ToBase64String(hashBytes);
         }
 
         public static byte[] GetSalt()
         {
             byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
             return salt;
         }
 
         public static bool Validate(string password, string userHashedPassword)
         {
             var hashBytes = Convert.FromBase64String(userHashedPassword);
-            byte[] salt = new byte[16];
-            Array.Copy(hashBytes, 0, salt, 0, 16);
+            byte[] salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
 
             var hash = GetHash(password, salt);
 
-            for (var i = 0; i < 20; i++)
+            for (var i = 0; i < HashSize; i++)
             {
-                if (hashBytes[i + 16] != hash[i])
+                if (hashBytes[i + SaltSize] != hash[i])
                 {
                     return false;
                 }
@@ -45,7 +47,7 @@ namespace Authentication.Core.Utilities
         private static byte[] GetHash(string password, byte[] salt)
         {
             var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
-            return pbkdf2.GetBytes(20);
+            return pbkdf2.GetBytes(HashSize);
         }
     }
 }
